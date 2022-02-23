68058691d3d7822eba7529f890b491951c8ca52d
kmk61003 <99633550+kmk61003@users.noreply.github.com>
2022-02-23 12:48:39 +0530

    Delete Q3.py

--- TASK-4/Q3.py
+++
@@ -1,8 +0,0 @@
-d = [ [1, 'x', 90],
-     [2, 'y', 95],
-     [3, 'z', 85]]
-     
-print("{:<10} {:<10} {:<10}".format('Roll no','name','marks'))
-
-for i in range(3):
-    print(d[1][i],end=" "*10)
